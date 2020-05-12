2f81206559609209190fd8ddd6619ca1d25a0cac
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-12 16:42:15 -0300

    Projeto criado

---
+++ src/main/java/com/coursejava/projetospringbootjpa/ProjetoSpringbootJpaApplication.java
@@ -0,0 +1,13 @@
+package com.coursejava.projetospringbootjpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjetoSpringbootJpaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjetoSpringbootJpaApplication.class, args);
+	}
+
+}
