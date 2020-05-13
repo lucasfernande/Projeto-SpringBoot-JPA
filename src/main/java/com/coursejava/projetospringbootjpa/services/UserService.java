6643111e0c07bed0cb97db2189937ef0f6321f19
lucasfernande <lucas.fernandes6192@outlook.com>
2020-05-13 11:58:12 -0300

    Camada de serviço e registro de componentes

---
+++ src/main/java/com/coursejava/projetospringbootjpa/services/UserService.java
@@ -0,0 +1,27 @@
+package com.coursejava.projetospringbootjpa.services;
+
+import java.util.List;
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.coursejava.projetospringbootjpa.entities.User;
+import com.coursejava.projetospringbootjpa.repositories.UserRepository;
+
+@Service // registrando a classe como um componente do spring
+
+public class UserService  {
+    
+	@Autowired
+	private UserRepository repo;
+	
+	public List<User> findAll() { // método que retorna todos os usuários do banco de dados
+		return repo.findAll();
+	}
+	
+	public User findById(Long id) {
+		Optional<User> user = repo.findById(id);
+		return user.get();
+	}
+}
